d057371c5d6dc7f9e32bb97a408c64fa9aba76d3
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2021-05-04 14:24:04 +0200

    Update DESCRIPTION

---
+++ R/khronos-package.R
@@ -0,0 +1,32 @@
+#' @details
+#' \tabular{ll}{
+#'  **Package:** \tab khronos \cr
+#'  **Type:** \tab Package \cr
+#'  **Version:** \tab 0.0.0.9000 \cr
+#'  **License:** \tab GPL-3 \cr
+#' }
+#'
+# \doi{xxx}
+#'
+#' @author
+#' **Full list of authors and contributors** (alphabetic order)
+#'
+#' \tabular{ll}{
+#'  Nicolas Frerebeau \tab *Université Bordeaux Montaigne, France* \cr
+#' }
+#'
+#' **Package maintainer**
+#'
+#' Nicolas Frerebeau\cr
+#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
+#'
+#' IRAMAT-CRP2A (UMR 5060)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' F-33607 Pessac cedex\cr
+#' France
+#' @name khronos-package
+#' @aliases khronos
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
