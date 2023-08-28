f0be14d84f4ebb314ceebf1b59b7152e2c4c3c14
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2023-08-28 10:11:11 +0200

    Small edits

--- R/coerce.R
+++ R/coerce.R
@@ -20,24 +20,24 @@ setMethod("as.data.frame", "MeanDate", as.data.frame.MeanDate)
 #' @export
 #' @method as.data.frame AoristicSum
 as.data.frame.AoristicSum <- function(x, ..., calendar = getOption("kairos.calendar")) {
-  methods::callNextMethod(x, ..., calendar = calendar)
+  block_start <- utils::head(x@breaks, -1)
+  block_end <- utils::tail(x@breaks, -1)
+
+  aorist <- x[, , 1, drop = TRUE]
+  dim(aorist) <- c(nrow(x), ncol(x))
+  colnames(aorist) <- colnames(x) %||% paste0("S", seq_len(ncol(x)))
+
+  data.frame(
+    start = aion::as_year(block_start, calendar = calendar),
+    end = aion::as_year(block_end, calendar = calendar),
+    aorist
+  )
 }
 
 #' @export
 #' @rdname data.frame
 #' @aliases as.data.frame,AoristicSum-method
 setMethod("as.data.frame", "AoristicSum", as.data.frame.AoristicSum)
-
-#' @export
-#' @method as.data.frame RateOfChange
-as.data.frame.RateOfChange <- function(x, ..., calendar = getOption("kairos.calendar")) {
-  methods::callNextMethod(x, ..., calendar = calendar)
-}
-
-#' @export
-#' @rdname data.frame
-#' @aliases as.data.frame,RateOfChange-method
-setMethod("as.data.frame", "RateOfChange", as.data.frame.RateOfChange)
 
 #' @method as.data.frame IncrementTest
 #' @export
