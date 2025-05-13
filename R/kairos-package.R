bb35ce337db6dd6b47d687b18274e83625c7afdb
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-05-13 19:06:46 +0200

    Update package metadata

--- R/kairos-package.R
+++ R/kairos-package.R
@@ -1,11 +1,16 @@
 #' @details
 #' \tabular{ll}{
-#'  **Package:** \tab kairos \cr
-#'  **Type:** \tab Package \cr
-#'  **Version:** \tab 2.2.1 \cr
-#'  **License:** \tab GPL-3 \cr
-#'  **Zenodo:** \tab \doi{10.5281/zenodo.5653896} \cr
+#'  **Version** \tab 2.2.1 \cr
+#'  **License** \tab GPL-3 \cr
+#'  **CRAN DOI** \tab \doi{10.32614/CRAN.package.kairos} \cr
+#'  **Zenodo DOI** \tab \doi{10.5281/zenodo.5653896} \cr
 #' }
+#'
+#' Archéosciences Bordeaux (UMR 6034)\cr
+#' Maison de l'Archéologie\cr
+#' Université Bordeaux Montaigne\cr
+#' F-33607 Pessac cedex\cr
+#' France
 #'
 #' @section Package options:
 #'  \pkg{kairos} uses the following [options()] to configure behavior:
@@ -14,26 +19,6 @@
 #'  * `kairos.verbose`: a [`logical`] scalar. Should \R report extra information
 #'    on progress? Defaults to [interactive()].
 #'
-#' @author
-#' **Full list of authors and contributors** (alphabetic order)
-#'
-#' \tabular{ll}{
-#'  Nicolas Frerebeau \tab *Université Bordeaux Montaigne, France* \cr
-#'  Brice Lebrun \tab  \cr
-#'  Ben Marwick \tab *University of Washington, USA* \cr
-#'  Anne Philippe \tab *Université de Nantes, France* \cr
-#' }
-#'
-#' **Package maintainer**
-#'
-#' Nicolas Frerebeau\cr
-#' \email{nicolas.frerebeau@@u-bordeaux-montaigne.fr}
-#'
-#' Archéosciences Bordeaux (UMR 6034)\cr
-#' Maison de l'Archéologie\cr
-#' Université Bordeaux Montaigne\cr
-#' F-33607 Pessac cedex\cr
-#' France
 #' @name kairos-package
 #' @aliases kairos
 #' @docType package
